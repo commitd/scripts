92914e09ff4c9735a83f038e719282f3005414e7
Chris Flatley <chris@committed.io>
2021-07-19 13:03:16 +0000

    Check package josn dir before using cwd or pwd

--- src/utils/package.ts
+++ src/utils/package.ts
@@ -5,7 +5,7 @@ import fs from "fs"
 const packageJsonDir = path.resolve(__dirname, "../..")
 
 export function findFileInPackage(...paths: string[]): string | undefined {
-  const scriptsBaseDir = process.env.INIT_CWD || packageJsonDir || process.cwd()
+  const scriptsBaseDir = packageJsonDir || process.env.INIT_CWD || process.cwd()
   const file = path.join(...[scriptsBaseDir, ...paths])
 
   if (!fs.existsSync(file)) {
